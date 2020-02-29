4c92f39b3214956f02092144f86995093606bf78
raghu55mirashi <raghu55mirashi@gmail.com>
2020-03-01 00:14:54 +0530

    urls page upd

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
