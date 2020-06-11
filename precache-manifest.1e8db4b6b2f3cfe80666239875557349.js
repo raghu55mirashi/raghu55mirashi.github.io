b02fa636492b6a00aa46bcb065530ba0b1d6aa35
raghu55mirashi <raghu55mirashi@gmail.com>
2020-06-11 17:11:47 +0530

    Updates

---
+++ precache-manifest.1e8db4b6b2f3cfe80666239875557349.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6613660590651b4a020c03a55a7ed6d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c85678d4a093b50a3738",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "76ff32ff2ba32c9dde7f",
+    "url": "/static/css/main.230906d4.chunk.css"
+  },
+  {
+    "revision": "c85678d4a093b50a3738",
+    "url": "/static/js/2.dada46ff.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.dada46ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ff32ff2ba32c9dde7f",
+    "url": "/static/js/main.c2dd6cf4.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "887243837f3a18bf9ace73f915db772f",
+    "url": "/static/media/thumb.88724383.png"
+  }
+]);
